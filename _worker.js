90dc7f053f987177e450b027f4d57871375c9d2b
leatok <zaheeramrb@gmail.com>
2026-01-03 14:42:10 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ export default {
 
 		const staticFiles = [
 			"favicon.ico",
-			"ajax/libs/jquery/3.6.0/jquery.min.js"
+			"ajax/libs/jquery/3.8.4/jquery.min.js"
 		];
 
 		if (staticFiles.includes(pathname.slice(1))) {
